55c7ed98d18367db26947c9d8b235b2a61f500cc
Fastattack <120670645+fastattackv@users.noreply.github.com>
2024-05-13 21:57:01 +0200

    Added files
    Added cliper namer.py and README.md

---
+++ README.md
@@ -0,0 +1,23 @@
+# OBS clip namer
+
+By fastattack, 2024, MIT license
+
+
+> [!NOTE]
+> The script is only working on Windows
+
+
+## Introduction
+This obs script organizes your replays: it names the replays by adding the game name at the start of the name of the file and it sorts the replays in folders that corresponds to the game the replay has been taken in.
+
+The name of the files will be changed to the name of the application running in fullscreen when the replay is saved, if there is not any app running in fullscreen, the replay will be sorted in the `unknown` folder.
+
+
+## Installation
+1. Install python ([python download](https://www.python.org/downloads/)) and link your installation with obs ([tutorial](https://www.youtube.com/watch?v=t7RhpvlVte0)).
+2. Install the required modules:
+   1. Install pywin 32 by running `pip install pywin32` in your command prompt. To do so, just launch the command prompt by searching for cmd and if you have installed python (you should have already installed it at this step) just copy `pip install pywin32` in the text area and press enter.
+3. Download the `clip namer.py` file by clicking [here](https://github.com/fastattackv/OBS-clip-namer/raw/main/clip%20namer.py).
+4. Copy the `clip namer.py` in the obs scripts folder (if you installed obs without changing its path, it should be `C:\Program Files\obs-studio\data\obs-plugins\frontend-tools\scripts`).
+5. Add the script in obs by clicking `tools` at the top of obs window, then press `scripts`, then in `scripts` press `+` and select the `clip namer.py` file.
+6. You are done ! If you encounter a bug or have a question about how to use the script, please use the github issues tab [here](https://github.com/fastattackv/OBS-clip-namer/issues). 
---
+++ clip namer.py
@@ -0,0 +1,82 @@
+""" Made by fastattack, 2024, MIT license
+Organizes obs output directory by naming the clips and moving them in folders corresponding to the game title
+"""
+
+import os
+import shutil
+import ctypes
+import win32gui
+
+import obspython as obs
+
+
+GetWindowText = ctypes.windll.user32.GetWindowTextW
+GetWindowTextLength = ctypes.windll.user32.GetWindowTextLengthW
+user32 = ctypes.windll.user32
+user32.SetProcessDPIAware()
+full_screen_rect = (0, 0, user32.GetSystemMetrics(0), user32.GetSystemMetrics(1))
+
+
+# obs funcs
+def script_description():
+    return "This script organize your output directory by naming the files and organizing them in the directory of the application they have been taken in\n\nMade by fastattack, 2024"
+
+
+def event_dispatch(event):
+    if event == obs.OBS_FRONTEND_EVENT_REPLAY_BUFFER_SAVED:
+        print("Replay saved")
+        replay_saved()
+
+
+def script_load(settings):
+    print("Loading script")
+    obs.obs_frontend_add_event_callback(event_dispatch)
+
+
+def script_unload():
+    print("Unloading script")
+    obs.obs_frontend_remove_event_callback(event_dispatch)
+
+
+# script funcs
+def get_fullscreen_hwnd() -> int | None:
+    """ Returns the fullscreen app's hwnd, if there is no fullscreen app, returns None """
+    try:
+        hwnd = user32.GetForegroundWindow()
+        rect = win32gui.GetWindowRect(hwnd)
+        if rect == full_screen_rect:
+            return hwnd
+        else:
+            return None
+    except:
+        return None
+
+
+def get_window_title_from_hwnd(hwnd):
+    length = GetWindowTextLength(hwnd)
+    buff = ctypes.create_unicode_buffer(length + 1)
+    GetWindowText(hwnd, buff, length + 1)
+    return buff.value
+
+
+def replay_saved():
+    """ Moves and renames a replay when it's saved to the game it has been captured in """
+    hwnd = get_fullscreen_hwnd()
+    if hwnd is not None:
+        game = get_window_title_from_hwnd(hwnd)
+    else:
+        game = "unknown"
+
+    file_path = obs.obs_frontend_get_last_replay()
+    file_directory = os.path.dirname(file_path)
+    file_name = os.path.basename(file_path)
+
+    if not os.path.isdir(os.path.join(file_directory, game)):  # there is not already a directory for this game
+        os.mkdir(os.path.join(file_directory, game))
+
+    try:
+        new_file_path = os.path.join(file_directory, game, f"{game} {file_name}")
+        shutil.move(file_path, new_file_path)
+        print(f"Saved replay to: {new_file_path}")
+    except Exception as e:
+        print(f"Unexpected error when saving replay: {e}")
